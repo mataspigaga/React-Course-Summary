e1d8c951fb85f3601cc0d8a259cde36bc34bbe38
Matas Pigaga <mataspigaga@gmail.com>
2021-08-24 15:18:45 -0400

    created a custom http hook

--- react-hooks-summary/src/components/Ingredients/Ingredients.js
+++ react-hooks-summary/src/components/Ingredients/Ingredients.js
@@ -4,6 +4,7 @@ import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
+import useHttp from "../../hooks/http";
 
 const ingredientReducer = (currentIngredients, action) => {
   switch (action.type) {
@@ -18,30 +19,9 @@ const ingredientReducer = (currentIngredients, action) => {
   }
 };
 
-const httpReducer = (curHttpState, action) => {
-  switch (action.type) {
-    case "SEND":
-      return { loading: true, error: null };
-    case "RESPONSE":
-      return { ...curHttpState, loading: false };
-    case "ERROR":
-      return { loading: false, error: action.errorMessage };
-    case "CLEAR":
-      return { ...curHttpState, error: null };
-    default:
-      throw new Error("Should not be reached!");
-  }
-};
-
 const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
-  const [httpState, dispatchHttp] = useReducer(httpReducer, {
-    loading: false,
-    error: null,
-  });
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [userIngredients, setUserIngredients] = useState([]);
-  // const [error, setError] = useState();
+  const { isLoading, error, data, sendRequest } = useHttp();
 
   // useEffect will run on the first render and then anytime anything in the dependencies array changes
 
@@ -55,63 +35,19 @@ const Ingredients = () => {
 
   // useCallback will cache the function on rerender
 
-  const addIngredientHandler = useCallback((ingredient) => {
-    // setIsLoading(true);
-    dispatchHttp({ type: "SEND" });
-    fetch(
-      "https://react-course-recap-default-rtdb.firebaseio.com/ingredients.json",
-      {
-        method: "POST",
-        body: JSON.stringify(ingredient),
-        headers: { "Content-Type": "application/json" },
-      }
-    )
-      .then((response) => {
-        // setIsLoading(false);
-        dispatchHttp({ type: "RESPONSE" });
-        return response.json();
-      })
-      .then((responseData) => {
-        // setUserIngredients((prevIngredients) => [
-        //   ...prevIngredients,
-        //   { id: responseData.name, ...ingredient },
-        // ]);
-        dispatch({
-          type: "ADD",
-          ingredient: { id: responseData.name, ...ingredient },
-        });
-      });
-  }, []);
+  const addIngredientHandler = useCallback((ingredient) => {}, []);
 
-  const removeIngredientHandler = useCallback((ingredientId) => {
-    // setIsLoading(true);
-    dispatchHttp({ type: "SEND" });
-    fetch(
-      `https://react-course-recap-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
-      { method: "DELETE" }
-    )
-      .then((response) => {
-        // setIsLoading(false);
-        dispatchHttp({ type: "RESPONSE" });
-        // setUserIngredients((prevIngredients) =>
-        //   prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
-        dispatch({
-          type: "DELETE",
-          id: ingredientId,
-        });
-      })
-      .catch((error) => {
-        // setError("Something went wrong!");
-        // setIsLoading(false);
-        dispatchHttp({ type: "ERROR", errorMessage: "Something went wrong!" });
-      });
-  }, []);
+  const removeIngredientHandler = useCallback(
+    (ingredientId) => {
+      sendRequest(
+        `https://react-course-recap-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
+        "DELETE"
+      );
+    },
+    [sendRequest]
+  );
 
-  const clearError = () => {
-    // setError(null);
-    // setIsLoading(false);
-    dispatchHttp({ type: "CLEAR" });
-  };
+  const clearError = () => {};
 
   const ingredientList = useMemo(() => {
     return (
@@ -124,13 +60,11 @@ const Ingredients = () => {
 
   return (
     <div className="App">
-      {httpState.error && (
-        <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>
-      )}
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
 
       <IngredientForm
         onAddIngredient={addIngredientHandler}
-        loading={httpState.loading}
+        loading={isLoading}
       />
 
       <section>
