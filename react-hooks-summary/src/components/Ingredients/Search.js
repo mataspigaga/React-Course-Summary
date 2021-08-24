3832d7171a4768f12d6188cf09f8cae122cf9aa2
Matas Pigaga <mataspigaga@gmail.com>
2021-08-24 12:34:52 -0400

    added timer to filter to lcean up search function

--- react-hooks-summary/src/components/Ingredients/Search.js
+++ react-hooks-summary/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -6,27 +6,35 @@ import "./Search.css";
 const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
   const [filter, setFilter] = useState("");
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query =
-      filter.length === 0 ? "" : `?orderBy="title"&equalTo="${filter}"`;
-    fetch(
-      "https://react-course-recap-default-rtdb.firebaseio.com/ingredients.json" +
-        query
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    const timer = setTimeout(() => {
+      if (filter === inputRef.current.value) {
+        const query =
+          filter.length === 0 ? "" : `?orderBy="title"&equalTo="${filter}"`;
+        fetch(
+          "https://react-course-recap-default-rtdb.firebaseio.com/ingredients.json" +
+            query
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadIngredients(loadedIngredients);
           });
-        }
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [onLoadIngredients, filter]);
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onLoadIngredients, filter, inputRef]);
 
   return (
     <section className="search">
@@ -34,6 +42,7 @@ const Search = React.memo((props) => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={filter}
             onChange={(e) => setFilter(e.target.value)}
